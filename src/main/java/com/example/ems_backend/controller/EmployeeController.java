5e5afbc84035b768a559f0c1d21cb2803c047cda
Rohith101coder <rohithvadla07@gmail.com>
2026-02-06 13:10:46 +0530

    cross origin issues solved

--- src/main/java/com/example/ems_backend/controller/EmployeeController.java
+++ src/main/java/com/example/ems_backend/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import com.example.ems_backend.service.EmployeeService;
 
 import lombok.AllArgsConstructor;
 
-
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
@@ -39,7 +40,7 @@ public class EmployeeController {
        return ResponseEntity.ok(employeeDto);
     }
 
-    @GetMapping("/all")
+    @GetMapping
     public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
         return ResponseEntity.ok(employeeService.getAllEmployees());
     }
